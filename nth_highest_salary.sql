b8ae282e561d501c9e58b6c8009a39a84e384aeb
Athira Das <athiradas.mec@gmail.com>
2018-04-15 19:07:46 -0500

    Create nth_highest_salary.sql

---
+++ nth_highest_salary.sql
@@ -0,0 +1,10 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+      # Write your MySQL query statement below.
+      SELECT Salary
+      FROM
+      (SELECT (SELECT COUNT(DISTINCT Salary) + 1 FROM Employee e2 WHERE e2.Salary > e1.Salary) AS Rank, Salary FROM Employee e1) t1
+     WHERE Rank = N LIMIT 1
+  );
+END
